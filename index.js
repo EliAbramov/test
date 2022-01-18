5c3e1dbd94859d426cdf7d167ddaf6910bc4c2a1
Eli <eli@myfxbook.com>
2022-01-18 17:59:28 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,107 @@
+// print alert to the browser
+// window.alert("Hello Studetns")
+
+// print log and error log to console
+console.log("Cat")
+console.error("Dog")
+
+// change the text of the element
+// document.getElementById("myTitle").innerHTML = "Studetns"
+// console.log(document.getElementById("myTitle").textContent)
+
+// EX
+// print alert with your full name
+// print to console, your city name
+// change the h1 title to superhero name
+
+
+
+// Data type
+
+let number1 = 7;
+let number2;
+let number3;
+
+number2 = 8;
+number2 = number1 + 5;
+
+console.log(number1)
+console.log(number2)
+
+let res = number2 * number2 - number1; // res = 12 * 12 - 7 => 137
+console.log(res)
+
+let name = "Dani Shovevani"
+console.log(name)
+
+let word = "word"
+console.log(name + " " + word)
+
+
+// EX 
+// Create 3 vars of any numbers
+// Calculate the avg of them and print to the console 
+// avg = sum/amount  
+// Create 2 vars one for your first name, second for your last name
+// Print to the console the combination of them with space between
+
+
+const id = 123456;
+// id = 5; // we will get error
+
+let avg = (5 + 6 + 7) / 3;
+
+
+// number1 = number1 + 2
+// number1 = 7 + 2 => 9
+number1 += 2;
+console.log(number1)
+
+// after changes number1 is 9
+// number1 = 9 - 2 => 7
+number1 -= 2;
+console.log(number1)
+
+// number1 += 1;  we add to number1 one
+number1++;
+console.log(number1)
+number1--;
+console.log(number1)
+
+// print the date
+console.log(new Date())
+
+// 
+console.log(Math.PI)
+
+// integer
+let integer = 45;
+let double = 27.3
+
+console.log("Ceil: " + Math.ceil(double))
+console.log("Floor: " + Math.floor(double))
+
+
+//  0123456789....
+// "Dani Shovevani"
+
+console.log("cahr at 0: " + name.charAt(0))
+console.log("name length: " + name.length)
+console.log("index of a: " + name.indexOf("a"))
+console.log("index of z: " + name.indexOf("z"))
+console.log("sub stirng (5): " + name.substring(5)) // start from 5
+console.log("sub stirng (0, 4): " + name.substring(0, 4)) // start from 0 end at 4 (not included)
+console.log("sub stirng length: " + name.substring(0, 4).length) // 4
+console.log("replace last name: " + name.replace("Shovevani", "Abramov")) 
+
+// EX 
+// Create string "Tomato Soup"
+// print the char at index 4
+// print the index of char "t"
+// print substring from 4 to 7
+// replace the word "Soup" to "Salat"
+
+
+
+
+
